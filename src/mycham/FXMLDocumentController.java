a76c351dc40f9ea576b6af9e585f52c9e95f2b90
Airdin93 <emil.johansen93@gmail.com>
2017-01-03 10:13:57 +0100

    Add teams
    -Ability to add teams to arraylist
    -Abillity to shuffle teams

--- src/mycham/FXMLDocumentController.java
+++ src/mycham/FXMLDocumentController.java
@@ -6,25 +6,52 @@
 package mycham;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
- * @author Nicolai
+ * @author Emil
  */
 public class FXMLDocumentController implements Initializable {
+    
+    ArrayList<String> teams = new ArrayList<String>();
     
     @FXML
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField addTeam;
+    
+    
+    @FXML
+    private void addTeam(ActionEvent event) {
+
+        teams.add(addTeam.getText());
+        addTeam.clear();
+        
+    }
+    
+    @FXML
+    private void testButton2(ActionEvent event) {
+
+        for(int i = 0; i < teams.size(); i++) {   
+        System.out.print(teams.get(i));
+}
+
+    }
+    
+    @FXML
+    private void shuffleButton(ActionEvent event) {
+
+        Collections.shuffle(teams);
+
     }
     
     @Override
