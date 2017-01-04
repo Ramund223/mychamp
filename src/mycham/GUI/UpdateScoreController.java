be8cbbd14f6e3ee731e5478ab0583405dbc93b75
CrystalOfLife <nicolai7@godmail.dk>
2017-01-04 09:23:13 +0100

    Put it up in three layar achitecture
    thats it

--- src/mycham/GUI/UpdateScoreController.java
+++ src/mycham/GUI/UpdateScoreController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mycham.GUI.controller;
+package mycham.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
