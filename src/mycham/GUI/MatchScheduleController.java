6e219c46370ee493f25c6ac37860b127ab1af6cb
Airdin93 <emil.johansen93@gmail.com>
2017-01-05 11:29:43 +0100

    Remade matchschedule template
    Remade matchschedule template

--- src/mycham/GUI/MatchScheduleController.java
+++ src/mycham/GUI/MatchScheduleController.java
@@ -12,6 +12,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 /**
@@ -21,17 +23,55 @@ import javafx.stage.Stage;
  */
 public class MatchScheduleController implements Initializable {
     
+    
     @FXML
     private Button matchScheduleClose;
+    
+    @FXML
+    private Label match1A;
+    
+    @FXML
+    private Label match2A;
+    
+    @FXML
+    private Label match3A;
+    
+    @FXML
+    private Label match4A;
+    
+    @FXML
+    private Label match5A;
+    
+    @FXML
+    private Label match6A;
+    
+    @FXML
+    private Label match7A;
+    
+    @FXML
+    private Label match8A;
+    
+    @FXML
+    private Label match9A;
+    
+    @FXML
+    private Label match10A;
+    
+    @FXML
+    private Label match11A;
+    
+    @FXML
+    private Label match12A;
+    
+    private MainViewController mainClass;
 
-   private MainViewController mainClass;
-
-   ObservableList<String> listTeams;
+    ObservableList<String> listTeams;
    
-   public MatchScheduleController() 
-   {
+    public MatchScheduleController() 
+    {
         listTeams = TeamModel.getInstance().getTeam();
-   }
+        
+    }
     
     @FXML
     private void matchScheduleClose(ActionEvent event) 
@@ -39,6 +79,22 @@ public class MatchScheduleController implements Initializable {
         Stage stage = (Stage) matchScheduleClose.getScene().getWindow();
         stage.close();
         
+    }
+    
+    private void labels()
+    {
+        match1A.setText("Hjemmebane: "+ "(" + listTeams.get(0) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(1) + ")");
+        match2A.setText("Hjemmebane: "+ "(" + listTeams.get(2) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(3) + ")");
+        match3A.setText("Hjemmebane: "+ "(" + listTeams.get(1) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(0) + ")");
+        match4A.setText("Hjemmebane: "+ "(" + listTeams.get(3) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(2) + ")");
+        match5A.setText("Hjemmebane: "+ "(" + listTeams.get(0) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(2) + ")");
+        match6A.setText("Hjemmebane: "+ "(" + listTeams.get(1) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(3) + ")");
+        match7A.setText("Hjemmebane: "+ "(" + listTeams.get(2) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(0) + ")");
+        match8A.setText("Hjemmebane: "+ "(" + listTeams.get(3) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(1) + ")");
+        match9A.setText("Hjemmebane: "+ "(" + listTeams.get(0) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(3) + ")");
+        match10A.setText("Hjemmebane: "+ "(" + listTeams.get(1) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(2) + ")");
+        match11A.setText("Hjemmebane: "+ "(" + listTeams.get(2) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(1) + ")");
+        match12A.setText("Hjemmebane: "+ "(" + listTeams.get(3) + ")" + " VS " + "Udebane: " + "(" + listTeams.get(0) + ")");
     }
     
     @FXML
@@ -53,6 +109,8 @@ public class MatchScheduleController implements Initializable {
         {
             System.out.println("EMPTY");
         }
+//        hjemmebaneTeam1.getColumns().add("MAKO");
+        labels();
     }
     
     /**
